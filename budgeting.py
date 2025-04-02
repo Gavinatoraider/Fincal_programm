436ffef2846c5a10ff5941dc5a2e77a1635b210d
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-04-02 18:18:01 +0000

    sdfg

--- budgeting.py
+++ budgeting.py
@@ -4,19 +4,23 @@ def budget(total_income):
     print("""
     Budget Choices
     1. Set Budget Limit
-    2. Compare Budget""")
+    2. Compare Budget
+    3. Exit""")
 
     budget_choice = input("Choose a Number: ")
 
     if budget_choice == "1":
-        pass
+        set_budget()
     elif budget_choice == "2":
+        compare_budget()
+    elif budget_choice == "3":
         pass
     else:
-        pass
+        print("Not an Option!")
+        budget()
 
 
-    def set_limit(total_income):
+    def set_budget(total_income):
         limits = {}
         for i in categories:
             limit_choice = input(f"What percent of your income do you want {i} limits to be?(Leave Blank To Not Use): ")
@@ -24,5 +28,9 @@ def budget(total_income):
                 limit_choice = (int(limit_choice)/100)*total_income
             except:
                 print("Not A Number")
-                set_limit()
+                set_budget()
             limits(i) = limit_choice
+        return limits
+
+    def compare_budget():
+        pass
