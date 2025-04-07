d978ca9e3c0112430cc4be99413752d8a176562f
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-04-07 19:43:57 +0000

    asdfbgh

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Financial program
+# Personal Finance Program
+
+## Project Description
+---
+The purpose of this project is to help the user manage their finances by letting them add, store and manage data using this program
+
+## Installation
+---
+Not used for this class
+
+## Execution and usage
+Paragraph describing how to use the project
+![image](relative path to the image NOTE: Image has to be saved in the repository REMINDER: Show what the project looks like when running)  
+
+## Used Technologies
+---
++ First library in the repository
+`pip install code for the lbrary`
+
++ Second library in the repository
+`pip install code for the lbrary`
+
++ Third library in the repository
+`pip install code for the lbrary`
+
+## Current Features
+---
++ First feature I am proud of
++ Second feature I am proud of
++ Third feature I am proud of
+
+## Contributions
+---
+Not used for this class
+
+## Contributors
+---
++ Person who helped - what they did
++ Person who helped - what they did
++ Person who helped - what they did
+
+## Author's Information
+---
+Short paragraph about you (Each member of the group)
+Potentially has contact information
+
+## Change Log
+---
+Not used for this class
+
+## License
+---
+Not used for this class
--- income_expenses.py
+++ income_expenses.py
@@ -1,5 +1,69 @@
 def income_expenses():
 
+    def add_income():
+        def get_date():
+            
+            thirty_one = ["01", "03", "05", "07", "08", "10", "12"]
+            thirty = ["04", "06", "09", "11"]
+
+            
+            
+            def get_month():
+                month = input("What month did you get the income(Cant be less than 0 or greater than 12(MM format)): ")
+
+                try:
+                    int(month)
+                except:
+                    print("Not a Number")
+                    get_month()
+
+                if month < 1 or month > 12:
+                    print("Not a Valid Month!")
+                    get_month()
+
+                return month
+            
+            def get_day(month):
+                day = input("What day did you get the income(Has to be a day in the month): ")
+
+                try:
+                    int(day)
+                except:
+                    print("Not a Number")
+                    get_day()
+                
+                
+                if month in thirty_one:
+                    if day > 31 or day < 0:
+                        print("Not a Valid Day")
+                        get_day()
+                elif month in thirty:
+                    if day > 30 or day < 0:
+                        print("Not a Valid Day")
+                        get_day()
+                elif month == "02":
+                    if day > 28 or day < 0:
+                        print("Not a Valid Day")
+                        get_day()
+                
+                return day
+
+            def get_year():
+                pass
+
+
+            month = get_month()
+            day = get_day(month)
+            year = get_year()
+        
+        get_date()
+        with open("user_income.csv", "a"):
+            pass
+    
+    
+    
+    
+    
     print("""
     Income Expenses Choices
     1. Add Income Entires
@@ -8,3 +72,15 @@ def income_expenses():
     4. Exit""")
 
     choice = input("Choose a Number: ")
+
+    if choice == "1":
+        add_income()
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        pass
+    else:
+        print("Not a Choice!")
+        income_expenses()
